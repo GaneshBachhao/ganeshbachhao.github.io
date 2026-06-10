04882782c7d293d0ebc7dc1239c032ec299c25a8
Ganesh Bachhao <Ganesh.Bachhao@live.com>
2026-06-11 01:57:15 +0530

    update profile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log("Portfolio Loaded");
+
+const titles = [
+  "Senior Full Stack Engineer",
+  ".NET Backend Developer",
+  "Senior .NET Developer",
+  ".NET & Azure Specialist",
+  "Cloud Solution Designer",
+  "Technology Lead",
+  "AI Powered Application Builder",
+  "Software/Technology Architect",
+];
+
+let titleIndex = 0;
+
+// const updateTitle = () => {
+//   document.getElementById("dynamic-title").textContent = titles[titleIndex];
+//   titleIndex = (titleIndex + 1) % titles.length;
+// };
+
+// updateTitle();
+// setInterval(updateTitle, 2500);
+
+setInterval(() => {
+  document.getElementById("dynamic-title").innerText = titles[titleIndex];
+
+  titleIndex++;
+
+  if (titleIndex >= titles.length) titleIndex = 0;
+}, 2500);
